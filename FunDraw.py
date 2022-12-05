c8d7bd6755eec22f0d5576d2bc6d0d7047e3872b
utkarsh0336 <110279358+utkarsh0336@users.noreply.github.com>
2022-12-05 23:19:02 +0530

    Add files via upload

---
+++ FunDraw.py
@@ -0,0 +1,16 @@
+# import turtle
+# c = turtle.Turtle()
+# turtle.Screen().bgcolor("green")
+# c.forward(100)
+# c.color("red")
+# c.forward(100)
+# for i in range(300):
+#     c.forward(i)
+#     c.left(45)
+#     c.backward(15)
+#     c.forward(i)
+#     c.color('yellow')
+#     c.color('red')
+
+
+# turtle.done()
